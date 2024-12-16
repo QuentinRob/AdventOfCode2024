6a2c158bfe34c5d7b7cc1dd7ad25bc54d8f9e6a1
Quentin Robcis <qrobcis@assystem.com>
2024-12-16 16:09:10 +0100

    🎉 Init

---
+++ cmd/historianHysteria.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// historianHysteriaCmd represents the historianHysteria command
+var historianHysteriaCmd = &cobra.Command{
+	Use:     "historianHysteria",
+	Aliases: []string{"1"},
+	Short:   "What is the total distance between your lists ?",
+	Long: `What is the total distance between your lists ?
+
+Compute the total distance between two lists.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(historianHysteriaCmd)
+}
