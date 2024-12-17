b955b0cedd3dd1c53fc5bfa3f06854386b4fb311
Quentin Robcis <qrobcis@assystem.com>
2024-12-17 11:20:39 +0100

    ✨ Finish day 2 part 2

--- cmd/redNosedReports.go
+++ cmd/redNosedReports.go
@@ -5,21 +5,38 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/QuentinRob/AdventOfCode2024/internal/redNosedReports"
 
 	"github.com/spf13/cobra"
 )
 
 var redNosedReportsCmd = &cobra.Command{
-	Use:   "redNosedReports",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:     "redNosedReports",
+	Aliases: []string{"2"},
+	Short:   "How many reports are safe?",
+	Long: `How many reports are safe?
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
+
+    The levels are either all increasing or all decreasing.
+    Any two adjacent levels differ by at least one and at most three.
+
+In the example above, the reports can be found safe or unsafe by checking those rules:
+
+    7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
+    1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
+    9 7 6 2 1: Unsafe because 6 2 is a decrease of 4.
+    1 3 2 4 5: Unsafe because 1 3 is increasing but 3 2 is decreasing.
+    8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
+    1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		inputPath, _ := cmd.Flags().GetString("input")
+
+		reports := redNosedReports.NewRedNosedReports(inputPath)
+
+		fmt.Println("Total safe reports: \t", reports.GetTotalSafeReports())
+		fmt.Println("Total safe problem dampener reports: \t", reports.GetTotalSafeProblemDampenerReports())
 	},
 }
 
--- internal/redNosedReports/main.go
+++ internal/redNosedReports/main.go
@@ -1,4 +1,133 @@
 package redNosedReports
 
+import (
+	"bufio"
+	"cmp"
+	"log"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
 type RedNosedReports struct {
+	Reports []Report
+}
+
+type Report struct {
+	Ratings             []int
+	Safe                bool
+	SafeProblemDampener bool
+}
+
+func NewRedNosedReports(path string) *RedNosedReports {
+	reports, err := parseInputFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &RedNosedReports{
+		Reports: reports,
+	}
+}
+
+func (r *RedNosedReports) GetTotalSafeReports() int {
+	count := 0
+
+	for _, report := range r.Reports {
+		if report.Safe {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (r *RedNosedReports) GetTotalSafeProblemDampenerReports() int {
+	count := 0
+
+	for _, report := range r.Reports {
+		if report.SafeProblemDampener {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (r *Report) computeSafeness() {
+	r.Safe = computeSafeSlice(r.Ratings)
+}
+
+func computeSafeSlice(r []int) bool {
+	isSafe := false
+	if !(slices.IsSorted(r) || slices.IsSortedFunc(r, func(a, b int) int {
+		return -1 * cmp.Compare(a, b)
+	})) {
+		isSafe = false
+		return isSafe
+	}
+
+	isSafe = true
+
+	for i, _ := range r {
+		if i+1 >= len(r) {
+			return isSafe
+		}
+
+		diff := int(math.Abs(float64(r[i+1]) - float64(r[i])))
+
+		if !(diff > 0 && diff <= 3) {
+			isSafe = false
+		}
+	}
+
+	return isSafe
+}
+
+func (r *Report) computeSafenessProblemDampener() {
+
+	r.SafeProblemDampener = false
+
+	for i, _ := range r.Ratings {
+		if r.SafeProblemDampener != true {
+			tmp := make([]int, len(r.Ratings))
+			copy(tmp, r.Ratings)
+			r.SafeProblemDampener = computeSafeSlice(slices.Delete(tmp, i, i+1))
+		}
+	}
+}
+
+func parseInputFile(path string) ([]Report, error) {
+	var reports []Report
+
+	readFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		reportLine := strings.Fields(fileScanner.Text())
+		report := Report{
+			Ratings: make([]int, len(reportLine)),
+			Safe:    false,
+		}
+
+		for i, rating := range reportLine {
+			report.Ratings[i], _ = strconv.Atoi(rating)
+		}
+
+		report.computeSafeness()
+		report.computeSafenessProblemDampener()
+
+		reports = append(reports, report)
+	}
+
+	readFile.Close()
+
+	return reports, nil
 }
